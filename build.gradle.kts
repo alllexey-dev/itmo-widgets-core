9f7fa1ca6da3dba5d418291a28878ada1920f6bc
alllexey <alllexey.dev@gmail.com>
2025-10-21 14:59:43 +0300

    Pre-publish changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,49 @@
 plugins {
+    id("com.vanniktech.maven.publish") version "0.34.0"
     kotlin("jvm") version "2.1.21"
 }
 
 group = "dev.alllexey"
-version = "1.0.0b"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
+}
+
+mavenPublishing {
+    coordinates(group as String, rootProject.name, version as String)
+
+    pom {
+        name.set("itmo-widgets-core")
+        description.set("A component of ITMO.Widgets applications.")
+        inceptionYear.set("2025")
+        url.set("https://github.com/alllexey-dev/itmo-widgets-core")
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://www.opensource.org/licenses/mit-license.php")
+                distribution.set("https://www.opensource.org/licenses/mit-license.php")
+            }
+        }
+        developers {
+            developer {
+                id.set("alllexey")
+                name.set("Aleksey Makarov")
+                url.set("https://github.com/alllexey-dev")
+            }
+        }
+        scm {
+            url.set("https://github.com/alllexey-dev/itmo-widgets-core")
+            connection.set("scm:git:git://github.com/alllexey-dev/itmo-widgets-core.git")
+            developerConnection.set("scm:git:ssh://git@github.com/alllexey-dev/itmo-widgets-core.git")
+        }
+    }
 }
 
 dependencies {
