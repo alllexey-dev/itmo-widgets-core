717d5870879f0c37bd0dfcf18dc25f0a3c9b7867
alllexey <alllexey.dev@gmail.com>
2026-03-18 17:23:52 +0300

    Other stuff, v1.1.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "dev.alllexey"
-version = "1.1.6"
+version = "1.1.7"
 
 repositories {
     mavenCentral()
@@ -47,7 +47,7 @@ mavenPublishing {
 }
 
 dependencies {
-    api("dev.alllexey:my-itmo-api:1.4.1")
+    api("dev.alllexey:my-itmo-api:1.4.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
     testImplementation(kotlin("test"))
 }
